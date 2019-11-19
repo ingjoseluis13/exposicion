ee0649625857f61f5894ad676f21f9c383f29ded
KiraLightSama <kira.vs.yagami@gmail.com>
2019-11-18 20:22:37 -0400

    sdf
    sdfds

--- app/DetalleIngreso.php
+++ app/DetalleIngreso.php
@@ -6,16 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class DetalleIngreso extends Model
 {
-    protected $table = 'detalle_ingresos';
-
-    protected $fillable = [
-        'id_ingreso',
-        'id_producto',
-        'cantidad',
-        'precio_compra',
-        'precio_venta'
-    ];
-
-
-    public $timestamps = false;
 }
